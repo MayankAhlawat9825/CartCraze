def7f39d168b80596ecc96e21b1057d45f0e10a2
Mayank Ahlawat <mayankahlawat2005@gmail.com>
2025-02-25 15:31:19 +0530

    "backend issue solved"

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Initialize Razorpay instance
-const Razorpay = require("razorpay");
 const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
